d31bc840a4394a00eb2a417c641096b2552e4fef
villahermosas3 <villahermosas3@berea.edu>
2024-04-07 23:14:49 -0400

    works on laptop

--- script.js
+++ script.js
@@ -37,13 +37,13 @@ let mouseConstraint = Matter.MouseConstraint.create(engine, {
 
 // MAN
 const Man = {
-    body: Matter.Bodies.rectangle(window.innerWidth/2 + 500, window.innerHeight/2 + 70 , .001, .001, {
+    body: Matter.Bodies.rectangle(window.innerWidth/2 + 200, window.innerHeight/2 + 50, .0000001, .000001, {
       isStatic: true,
       render: {
         sprite: {
           texture: 'man.png', // Image for the plate
-          xScale: .7, // Adjust scale if needed
-          yScale: .7,
+          xScale: .6, // Adjust scale if needed
+          yScale: .6,
           sensor: true,
         }
       }
@@ -54,7 +54,7 @@ const Man = {
 
 //PLATE
 const plateBottom = {
-    body: Matter.Bodies.rectangle(window.innerWidth/4 + 800, window.innerHeight/2 - 175, 450, 10, {
+    body: Matter.Bodies.rectangle(window.innerWidth/2, window.innerHeight/2 - 175, 480, 10, {
       isStatic: true,
       angle: ( 15 * Math.PI) / 180,
       render: {
@@ -68,37 +68,95 @@ const plateBottom = {
     name: "plateBottom",
   };
 
-//STOMACH
+//Face
+const Face = {
+    body: Bodies.rectangle(window.innerWidth/2 + 315, window.innerHeight/2 - 90, 50, 8, {
+        isStatic: true,
+        angle: ( 90 * Math.PI) / 180,
+        render: {
+            visible: false,
+        }
+    }),
+    name: "Face",
+};
 
+
+//Tongue
+  const Tongue = {
+    body: Bodies.rectangle(window.innerWidth/2 + 270, window.innerHeight/2 - 50, 70, 8, {
+        isStatic: true,
+        angle: ( 20 * Math.PI) / 180,
+        render: {
+            visible: false,
+        }
+    }),
+    name: "Tongue",
+};
+
+const Throat = {
+    body: Bodies.rectangle(window.innerWidth/2 + 290, window.innerHeight/2, 70, 8, {
+        isStatic: true,
+        angle: ( 90 * Math.PI) / 180,
+        render: {
+            visible: false,
+        }
+    }),
+    name: "Throat",
+};
+
+const Throat2 = {
+    body: Bodies.rectangle(window.innerWidth/2 + 340, window.innerHeight/2 - 50, 60, 8, {
+        isStatic: true,
+        angle: ( 40 * Math.PI) / 180,
+        render: {
+            visible: false,
+        }
+    }),
+    name: "Throat2",
+};
+
+
+//STOMACH
 const stomachTop = {
-    body: Bodies.rectangle(window.innerWidth/2 + 450, window.innerHeight/2 + 130, 330, 10, {
+    body: Bodies.rectangle(window.innerWidth/2 + 150, window.innerHeight/2 + 110, 320, 10, {
         isStatic: true,
         angle: ( -25 * Math.PI) / 180,
+        render: {
+            visible: false,
+        }
     }),
     name: "stomachTop",
 };
 
 const stomachBottom = {
-    body: Bodies.rectangle(window.innerWidth/2 + 400, window.innerHeight - 90, 330, 10, {
+    body: Bodies.rectangle(window.innerWidth/2 + 100, window.innerHeight - 40, 300, 10, {
         isStatic: true,
         angle: ( -3 * Math.PI) / 180,
+        render: {
+            visible: false,
+        }
     }),
     name: "stomachBottom",
 };
 
 const stomachLeft = {
-    body: Bodies.rectangle(window.innerWidth/2 + 260, window.innerHeight - 190, 10, 200, {
+    body: Bodies.rectangle(window.innerWidth/2 - 20, window.innerHeight - 100, 10, 150, {
         isStatic: true,
         angle: ( 20 * Math.PI) / 180,
+        render: {
+            visible: false,
+        }
     }),
     name: "stomachLeft"
 };
 
 const stomachRight = {
-    body: Bodies.rectangle(window.innerWidth/2 + 630, window.innerHeight - 300, 10, 400, {
+    body: Bodies.rectangle(window.innerWidth/2 + 310, window.innerHeight - 220, 10, 350, {
         isStatic: true,
-        angle: (15 * Math.PI) / 180
-        
+        angle: (17 * Math.PI) / 180,
+        render: {
+            visible: false,
+        }
     }),
     name: "stomachRight"
 };
@@ -199,7 +257,10 @@ const Foods = [
 
 function createFood(texture, calories, size) {
     return function() {
-        var FoodBody = Bodies.circle(window.innerWidth/4, 200, size, {
+        var FoodBody = Bodies.circle(window.innerWidth/2, 0, size, {
+            restitution: 0.6,
+            friction: 0.1,
+            gravity: 1,
             render: {
                 sprite: {
                     texture: texture
@@ -284,12 +345,16 @@ var walls = [
 
 // Combine all bodies into a single array
 var bodiesToAdd = walls.map(wall => wall.body)
-    .concat(Man.body)
     .concat(stomachBottom.body)   
     .concat(stomachLeft.body)
     .concat(stomachTop.body)
+    .concat(Tongue.body)
+    .concat(Throat.body)
+    .concat(Throat2.body)
     .concat(stomachRight.body)
     .concat(plateBottom.body)
+    .concat(Man.body)
+    .concat(Face.body)
     .concat(mouseConstraint);
 
 // Add all bodies to the world
